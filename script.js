0703476f5b1ac9371539d944b2341427e1c8d032
Vinicius Bassalobre <bassalobre.vinicius@gmail.com>
2023-10-24 19:40:18 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,18 @@
 // Aguarde o carregamento completo da página
 document.addEventListener("DOMContentLoaded", function () {
-  console.log("INIT force-input-required.js");
-  var forms = document.getElementsByTagName("form");
-  console.log("forms", forms);
-  for (var i = 0; i < forms.length; i++) {
-    var form = forms[i];
-    var inputs = form.getElementsByTagName("input");
-    console.log("inputs", forms);
-    for (var j = 0; j < inputs.length; j++) {
-      var input = inputs[j];
-      console.log("inpt", input);
-      input.setAttribute("required", "true");
+  setTimeout(() => {
+    console.log("INIT force-input-required.js");
+    var forms = document.getElementsByTagName("form");
+    console.log("forms", forms);
+    for (var i = 0; i < forms.length; i++) {
+        var form = forms[i];
+        var inputs = form.getElementsByTagName("input");
+        console.log("inputs", forms);
+        for (var j = 0; j < inputs.length; j++) {
+        var input = inputs[j];
+        console.log("inpt", input);
+        input.setAttribute("required", "true");
+        }
     }
-  }
+  },2000);
 });
